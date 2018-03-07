63986d9b2f875e868f8888dcbe46190e6b64162c
Michal Pasz <michal.pasz@electropoli.pl>
2018-03-07 09:24:16 +0100

    zapytanie z Analizą ryzyka

--- SoftLab/Update_marketing_zof.sql
+++ SoftLab/Update_marketing_zof.sql
@@ -2,3 +2,24 @@
 select * from wusr_vv_mg_ZapOfertowe_new_OGF (nolock)
 
 
+
+SELECT 
+AnRyzBHP,
+AnRyzJak,
+AnRyzDP,
+AnRyzLog,
+AnRyzFin,
+T1.Data
+--,* 
+FROM wusr_vv_mg_ZapOfertowe_new_OGF t0 (nolock)
+	INNER JOIN mg_nagdow t1 (nolock) ON t0.nagid = t1.nagid
+WHERE 
+t1.Data < GetDate()
+AND 
+(AnRyzBHP IS NULL OR AnRyzJak  IS NULL OR AnRyzDP  i IS NULL OR AnRyzLog  IS NULL OR AnRyzFin  IS NULL )
+
+
+BEGIN TRAN
+
+ROLLBACK TRAN
+COMMIT TRAN
