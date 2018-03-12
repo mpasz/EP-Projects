3ec6689ba6f2388589ccded5565809445b2c8e7c
Michal Pasz <michal.pasz@electropoli.pl>
2018-03-12 08:34:07 +0100

    Rekonfiguracja plików_config itp

--- psFromServer/checkExists.ps1
+++
@@ -1,7 +0,0 @@
-﻿
- $TASK_PATH = '\'
-$TASK_NAME = 'Zlecenia nie planowane'
-$task = Get-ScheduledTask -TaskName $TASK_NAME -TaskPath $TASK_PATH 
-if($task.State -eq 'Ready'){
-    $task | Start-ScheduledTask
-}  
